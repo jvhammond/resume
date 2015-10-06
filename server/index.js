2eb5ec73131bb979005c33bbd4cf41d500ff8a10
Jeff Hammond <jhammond@underarmour.com>
2015-10-05 23:17:34 -0400

    serve correct files, update version

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ var express = require('express');
 var app = express();
 
 var oneDay = 86400000;
+var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+app.use(express.static(__dirname + '/../public', { maxAge: oneDay }));
 
 app.listen(process.env.PORT || 3000);
+
+console.log('server listening on port: ' + port);
