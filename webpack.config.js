10c2c0e82e418dc88403356b9867ebdc376f210a
Jeff Hammond <jefhammond@paypal.com>
2018-06-28 17:23:40 -0400

    adding webpack wip

--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,6 @@ const VENDOR_LIBS = [
 ];
 
 module.exports = {
-  entry: {
-    bundle: './src/index.js',
-    vendor: VENDOR_LIBS
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js'
-  },
   module: {
     rules: [
       {
@@ -33,11 +25,8 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     }),
-    new webpack.optimize.CommonsChunkPlugin({
-      names: ['vendor', 'manifest']
-    }),
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: 'index.html'
     })
   ]
 };
